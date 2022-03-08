5237d9cb06479faf10da22fa40ae667bdae2e7b5
TheMagicHand777 <TheMagicHand777@gmail.com>
2022-03-08 10:51:58 -0800

    initial commit

---
+++ contracts/NFTContract.sol
@@ -0,0 +1,126 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTContract is ERC1155, Ownable, ReentrancyGuard{
+    
+    using Counters for Counters.Counter;
+    using SafeMath for uint256;
+
+    uint256 MAX_TOKEN_SIZE = 1000;
+    uint256 MAX_COUNT_PER_TOKEN = 30;
+
+    // Minting Prices Per stage
+    uint256 _privateSalePrice = 0.0001 ether; 
+    uint256 _publicSalePrice = 0.0002 ether; 
+
+    // used to validate whitelists
+    bytes32 public whitelistMerkleRoot;
+    Counters.Counter private _tokenIdTracker;
+
+    string  private _baseUri = "";
+    uint256 private currentSupply = 0;
+    /**
+     * @dev validates merkleProof
+     */
+    modifier isValidMerkleProof(bytes32[] calldata merkleProof, bytes32 root) {
+        require(
+            MerkleProof.verify(
+                merkleProof,
+                root,
+                keccak256(abi.encodePacked(msg.sender))
+            ),
+            "Address does not exist in list"
+        );
+        _;
+    }
+
+    modifier isCorrectPayment(uint256 price, uint256 numberOfTokens) {
+        require(
+            price * numberOfTokens == msg.value,
+            "Incorrect ETH value sent"
+        );
+        _;
+    }
+
+    constructor(string memory  baseUri) ERC1155("") {    
+        _baseUri = baseUri;
+    }
+    
+    function testMint() public onlyOwner {
+        _tokenIdTracker.increment();
+        _mint(msg.sender, _tokenIdTracker.current(), 1, "");
+        _tokenIdTracker.increment();
+        _mint(msg.sender, _tokenIdTracker.current(), 1, "");
+    }
+
+    function uri( uint256 _id) public view override returns (string memory) {
+        return string(abi.encodePacked(_baseUri, Strings.toString(_id)));
+    }
+    //mint nfts (only owner of the smart contract can mint nfts)
+    function mint(address account, uint256 id, uint256 amount) public onlyOwner {
+        _mint(account, id, amount, "");
+    }
+
+    //Burn nfts (only owner of the nft can burn nft)
+    function burn(address account, uint256 id, uint256 amount) public {
+        require(msg.sender == account);
+        _burn(account, id, amount);
+    }
+
+    function preSale(uint256 mintCount, bytes32[] calldata merkleProof)
+        external
+        payable
+        isValidMerkleProof(merkleProof, whitelistMerkleRoot)
+        isCorrectPayment(_privateSalePrice, mintCount)
+    {
+        require(currentSupply + mintCount <= MAX_TOKEN_SIZE, "Overflow Max Supply");
+        for(uint i = 0; i < mintCount; i++) {
+            _tokenIdTracker.increment();
+            _mint(msg.sender, _tokenIdTracker.current(), 1, "");
+        }
+        currentSupply = currentSupply + mintCount;
+    }
+
+    function publicSale(uint256 mintCount, bytes32[] calldata merkleProof)
+        external
+        payable
+        isValidMerkleProof(merkleProof, whitelistMerkleRoot)
+        isCorrectPayment(_publicSalePrice, mintCount)
+    {
+        require(currentSupply + mintCount <= MAX_TOKEN_SIZE, "Overflow Max Supply");
+        for(uint i = 0; i < mintCount; i++) {
+            _tokenIdTracker.increment();
+            _mint(msg.sender, _tokenIdTracker.current(), 1, "");
+        }
+        currentSupply = currentSupply + mintCount;
+    }
+
+
+    function setWhitelistMerkleRoot(bytes32 merkleRoot) external onlyOwner {
+        whitelistMerkleRoot = merkleRoot;
+    }
+
+    function setPrivateSalePrice(uint256 value) public onlyOwner {
+        _privateSalePrice = value;
+    }
+
+    function getPrivateSalePrice() public view returns (uint256) {
+        return _privateSalePrice;
+    }
+
+    function setPublicSalePrice(uint256 value) public onlyOwner {
+        _publicSalePrice = value;
+    }
+
+    function getPublicSalePrice() public view returns (uint256) {
+        return _publicSalePrice;
+    }
+}
---
+++ scripts/merkleTree.js
@@ -0,0 +1,19 @@
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
+const giftList = require('../whitelist/whitelist.json');
+
+const hashedAddresses = giftList.map(addr => keccak256(addr));
+const merkleTree = new MerkleTree(hashedAddresses, keccak256, { sortPairs: true });
+
+const myAddress = "0xD6e842B844a67151D9319CFED96B39EEeC6D466f";
+const hashedAddress = keccak256(myAddress);
+const proof = merkleTree.getHexProof(hashedAddress);
+const root = merkleTree.getHexRoot();
+
+// just for front-end display convenience
+// proof will be validated in smart contract as well
+const valid = merkleTree.verify(proof, hashedAddress, root);
+
+console.log(proof);
+console.log(root);
+console.log(valid);
